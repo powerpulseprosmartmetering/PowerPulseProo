978e2aa2db340bda6546d7014a1e0fcd1e724929
powerpulseprosmartmetering <powerpulsepro.smartmetering@gmail.com>
2026-05-01 01:08:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -101,7 +101,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("🚀 MongoDB connected successfully"))
   .catch(err => console.log("❌ MongoDB connection error:", err));
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🌟 Server running on port ${PORT}`);
